5dca04d39b278b0cf221010cff12e239235b22bc
Charles Dunn <dunnc1551@gmail.com>
2017-03-20 22:00:59 -0600

    Add film component

---
+++ src/components/Film/Film.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Film.css'
+
+const Film = ({ film }) => {
+  return (
+    <div className="film-wrap">
+      <div className="crawl">
+        <p>{film.title}</p>
+        <p>{film.opening_crawl}</p>
+        <p>{film.release_date}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Film;
