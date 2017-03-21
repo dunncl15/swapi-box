4d741992c7771580f1e7e567e41844b3e783fdbc
Charles Dunn <dunnc1551@gmail.com>
2017-03-20 21:57:28 -0600

    Add state properties, fetchData function to make API call, pass fetchData as a prop to each button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,86 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Buttons from './components/Buttons/Buttons.js'
+
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      openingFilm: {},
+      people: [],
+      planets: [],
+      vehicles: []
+    }
+  }
+
+  fetchData(value, e) {
+    fetch(`http://swapi.co/api/${value}`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      switch (value) {
+        case 'people':
+          data.results.map(person => {
+            const { name, homeworld, species, population_of_homeworld } = person;
+            this.setState({ people: this.state.people.concat([{
+              name,
+              homeworld,
+              species,
+              population_of_homeworld
+            }]) })
+          });
+          break;
+        case 'planets':
+          data.results.map(planet => {
+            const { name, terrain, population, climate, residents } = planet
+            this.setState({ planets: this.state.planets.concat([{
+              name,
+              terrain,
+              population,
+              climate,
+              residents
+            }]) })
+          });
+          break;
+        case 'vehicles':
+          data.results.map(vehicle => {
+            const { name, model, vehicle_class, number_of_passengers} = vehicle;
+            this.setState({ vehicles: this.state.vehicles.concat([{
+              name,
+              model,
+              vehicle_class,
+              number_of_passengers
+            }]) })
+          });
+          break;
+        }
+      })
+  }
+
+  componentDidMount() {
+  fetch('http://swapi.co/api/films')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    const index = Math.floor(Math.random() * data.count);
+    const { opening_crawl, release_date, title } = data.results[index];
+    this.setState({
+      openingFilm: { opening_crawl, release_date, title }
+    })
+  })
+}
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to SWAPIbox</h2>
+          <h2>Swapibox</h2>
         </div>
+        <Buttons fetchData={(type, e) => this.fetchData(type, e)} film={this.state.openingFilm} />
       </div>
     );
   }
